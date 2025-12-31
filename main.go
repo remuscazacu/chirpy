909decd2dea9290ba4fa56f42fcd89893850ee0e
remuscazacu <remus.cazacu@bitsoftware.ro>
2025-12-31 14:34:20 +0200

    add HandleFunc for /healthx check and /app entry point

--- main.go
+++ main.go
@@ -4,8 +4,17 @@ import (
 	"net/http"
 )
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 
 	server := &http.Server{
 		Addr:    ":8080",
